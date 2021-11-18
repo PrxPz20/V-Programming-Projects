bf6e05aa26feb5db398da1ca540a24e4360907f3
Nikolas <ntzantidis.n@live.unic.ac.cy>
2021-11-18 20:48:37 +0200

    --

--- The Concentration Game/MainPage.Designer.cs
+++ The Concentration Game/MainPage.Designer.cs
@@ -31,7 +31,6 @@ namespace The_Concentration_Game
         {
             this.components = new System.ComponentModel.Container();
             this.cardsGroupBox = new System.Windows.Forms.GroupBox();
-            this.cardTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.gameStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -45,35 +44,42 @@ namespace The_Concentration_Game
             this.match3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.hideImageTimer = new System.Windows.Forms.Timer(this.components);
+            this.CountDownLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.card1PictureBox = new System.Windows.Forms.PictureBox();
+            this.cardFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.card2PictureBox = new System.Windows.Forms.PictureBox();
+            this.card3PictureBox = new System.Windows.Forms.PictureBox();
+            this.card_4_PictureBox = new System.Windows.Forms.PictureBox();
+            this.card_5_PictureBox = new System.Windows.Forms.PictureBox();
+            this.card_6_PictureBox = new System.Windows.Forms.PictureBox();
+            this.card_7_PictureBox = new System.Windows.Forms.PictureBox();
+            this.card_8_PictureBox = new System.Windows.Forms.PictureBox();
+            this.card_9_PictureBox = new System.Windows.Forms.PictureBox();
+            this.card_10_PictureBox = new System.Windows.Forms.PictureBox();
             this.cardsGroupBox.SuspendLayout();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.card1PictureBox)).BeginInit();
+            this.cardFlowLayoutPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.card2PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card3PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_4_PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_5_PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_6_PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_7_PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_8_PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_9_PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_10_PictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // cardsGroupBox
             // 
-            this.cardsGroupBox.Controls.Add(this.cardTableLayoutPanel);
+            this.cardsGroupBox.Controls.Add(this.cardFlowLayoutPanel);
             this.cardsGroupBox.Location = new System.Drawing.Point(0, 27);
             this.cardsGroupBox.Name = "cardsGroupBox";
-            this.cardsGroupBox.Size = new System.Drawing.Size(671, 237);
+            this.cardsGroupBox.Size = new System.Drawing.Size(353, 198);
             this.cardsGroupBox.TabIndex = 0;
             this.cardsGroupBox.TabStop = false;
-            // 
-            // cardTableLayoutPanel
-            // 
-            this.cardTableLayoutPanel.ColumnCount = 5;
-            this.cardTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.cardTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.cardTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.cardTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.cardTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.cardTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.cardTableLayoutPanel.Location = new System.Drawing.Point(3, 16);
-            this.cardTableLayoutPanel.Name = "cardTableLayoutPanel";
-            this.cardTableLayoutPanel.RowCount = 2;
-            this.cardTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.cardTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.cardTableLayoutPanel.Size = new System.Drawing.Size(665, 218);
-            this.cardTableLayoutPanel.TabIndex = 0;
             // 
             // menuStrip1
             // 
@@ -84,7 +90,7 @@ namespace The_Concentration_Game
             this.aboutStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(683, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(828, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -165,12 +171,132 @@ namespace The_Concentration_Game
             this.aboutStripMenuItem.Text = "About";
             this.aboutStripMenuItem.Click += new System.EventHandler(this.aboutStripMenuItem_Click);
             // 
+            // CountDownLabel
+            // 
+            this.CountDownLabel.AutoSize = true;
+            this.CountDownLabel.Location = new System.Drawing.Point(724, 43);
+            this.CountDownLabel.Name = "CountDownLabel";
+            this.CountDownLabel.Size = new System.Drawing.Size(35, 13);
+            this.CountDownLabel.TabIndex = 2;
+            this.CountDownLabel.Text = "label1";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(232, 325);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "button1";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // card1PictureBox
+            // 
+            this.card1PictureBox.Location = new System.Drawing.Point(6, 6);
+            this.card1PictureBox.Name = "card1PictureBox";
+            this.card1PictureBox.Size = new System.Drawing.Size(60, 60);
+            this.card1PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.card1PictureBox.TabIndex = 0;
+            this.card1PictureBox.TabStop = false;
+            // 
+            // cardFlowLayoutPanel
+            // 
+            this.cardFlowLayoutPanel.Controls.Add(this.card1PictureBox);
+            this.cardFlowLayoutPanel.Controls.Add(this.card2PictureBox);
+            this.cardFlowLayoutPanel.Controls.Add(this.card3PictureBox);
+            this.cardFlowLayoutPanel.Controls.Add(this.card_4_PictureBox);
+            this.cardFlowLayoutPanel.Controls.Add(this.card_5_PictureBox);
+            this.cardFlowLayoutPanel.Controls.Add(this.card_6_PictureBox);
+            this.cardFlowLayoutPanel.Controls.Add(this.card_7_PictureBox);
+            this.cardFlowLayoutPanel.Controls.Add(this.card_8_PictureBox);
+            this.cardFlowLayoutPanel.Controls.Add(this.card_9_PictureBox);
+            this.cardFlowLayoutPanel.Controls.Add(this.card_10_PictureBox);
+            this.cardFlowLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cardFlowLayoutPanel.Location = new System.Drawing.Point(3, 16);
+            this.cardFlowLayoutPanel.Name = "cardFlowLayoutPanel";
+            this.cardFlowLayoutPanel.Padding = new System.Windows.Forms.Padding(3);
+            this.cardFlowLayoutPanel.Size = new System.Drawing.Size(347, 179);
+            this.cardFlowLayoutPanel.TabIndex = 0;
+            // 
+            // card2PictureBox
+            // 
+            this.card2PictureBox.Location = new System.Drawing.Point(72, 6);
+            this.card2PictureBox.Name = "card2PictureBox";
+            this.card2PictureBox.Size = new System.Drawing.Size(60, 60);
+            this.card2PictureBox.TabIndex = 1;
+            this.card2PictureBox.TabStop = false;
+            // 
+            // card3PictureBox
+            // 
+            this.card3PictureBox.Location = new System.Drawing.Point(138, 6);
+            this.card3PictureBox.Name = "card3PictureBox";
+            this.card3PictureBox.Size = new System.Drawing.Size(60, 60);
+            this.card3PictureBox.TabIndex = 2;
+            this.card3PictureBox.TabStop = false;
+            // 
+            // card_4_PictureBox
+            // 
+            this.card_4_PictureBox.Location = new System.Drawing.Point(204, 6);
+            this.card_4_PictureBox.Name = "card_4_PictureBox";
+            this.card_4_PictureBox.Size = new System.Drawing.Size(60, 60);
+            this.card_4_PictureBox.TabIndex = 3;
+            this.card_4_PictureBox.TabStop = false;
+            // 
+            // card_5_PictureBox
+            // 
+            this.card_5_PictureBox.Location = new System.Drawing.Point(270, 6);
+            this.card_5_PictureBox.Name = "card_5_PictureBox";
+            this.card_5_PictureBox.Size = new System.Drawing.Size(60, 60);
+            this.card_5_PictureBox.TabIndex = 4;
+            this.card_5_PictureBox.TabStop = false;
+            // 
+            // card_6_PictureBox
+            // 
+            this.card_6_PictureBox.Location = new System.Drawing.Point(6, 72);
+            this.card_6_PictureBox.Name = "card_6_PictureBox";
+            this.card_6_PictureBox.Size = new System.Drawing.Size(60, 60);
+            this.card_6_PictureBox.TabIndex = 5;
+            this.card_6_PictureBox.TabStop = false;
+            // 
+            // card_7_PictureBox
+            // 
+            this.card_7_PictureBox.Location = new System.Drawing.Point(72, 72);
+            this.card_7_PictureBox.Name = "card_7_PictureBox";
+            this.card_7_PictureBox.Size = new System.Drawing.Size(60, 60);
+            this.card_7_PictureBox.TabIndex = 6;
+            this.card_7_PictureBox.TabStop = false;
+            // 
+            // card_8_PictureBox
+            // 
+            this.card_8_PictureBox.Location = new System.Drawing.Point(138, 72);
+            this.card_8_PictureBox.Name = "card_8_PictureBox";
+            this.card_8_PictureBox.Size = new System.Drawing.Size(60, 60);
+            this.card_8_PictureBox.TabIndex = 7;
+            this.card_8_PictureBox.TabStop = false;
+            // 
+            // card_9_PictureBox
+            // 
+            this.card_9_PictureBox.Location = new System.Drawing.Point(204, 72);
+            this.card_9_PictureBox.Name = "card_9_PictureBox";
+            this.card_9_PictureBox.Size = new System.Drawing.Size(60, 60);
+            this.card_9_PictureBox.TabIndex = 8;
+            this.card_9_PictureBox.TabStop = false;
+            // 
+            // card_10_PictureBox
+            // 
+            this.card_10_PictureBox.Location = new System.Drawing.Point(270, 72);
+            this.card_10_PictureBox.Name = "card_10_PictureBox";
+            this.card_10_PictureBox.Size = new System.Drawing.Size(60, 60);
+            this.card_10_PictureBox.TabIndex = 9;
+            this.card_10_PictureBox.TabStop = false;
+            // 
             // MainPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ClientSize = new System.Drawing.Size(683, 450);
+            this.ClientSize = new System.Drawing.Size(828, 450);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.CountDownLabel);
             this.Controls.Add(this.cardsGroupBox);
             this.Controls.Add(this.menuStrip1);
             this.Name = "MainPage";
@@ -178,6 +304,17 @@ namespace The_Concentration_Game
             this.cardsGroupBox.ResumeLayout(false);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.card1PictureBox)).EndInit();
+            this.cardFlowLayoutPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.card2PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card3PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_4_PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_5_PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_6_PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_7_PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_8_PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_9_PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_10_PictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -186,7 +323,6 @@ namespace The_Concentration_Game
         #endregion
 
         private System.Windows.Forms.GroupBox cardsGroupBox;
-        private System.Windows.Forms.TableLayoutPanel cardTableLayoutPanel;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem gameStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
@@ -200,6 +336,19 @@ namespace The_Concentration_Game
         private System.Windows.Forms.ToolStripMenuItem match2ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem match3ToolStripMenuItem;
         private System.Windows.Forms.Timer hideImageTimer;
+        private System.Windows.Forms.Label CountDownLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.FlowLayoutPanel cardFlowLayoutPanel;
+        private System.Windows.Forms.PictureBox card1PictureBox;
+        private System.Windows.Forms.PictureBox card2PictureBox;
+        private System.Windows.Forms.PictureBox card3PictureBox;
+        private System.Windows.Forms.PictureBox card_4_PictureBox;
+        private System.Windows.Forms.PictureBox card_5_PictureBox;
+        private System.Windows.Forms.PictureBox card_6_PictureBox;
+        private System.Windows.Forms.PictureBox card_7_PictureBox;
+        private System.Windows.Forms.PictureBox card_8_PictureBox;
+        private System.Windows.Forms.PictureBox card_9_PictureBox;
+        private System.Windows.Forms.PictureBox card_10_PictureBox;
     }
 }
 
--- The Concentration Game/MainPage.cs
+++ The Concentration Game/MainPage.cs
@@ -13,58 +13,178 @@ namespace The_Concentration_Game
     public partial class MainPage : Form
     {
 
+        bool allowClick = false; // Allows user to choose and click on first card only after they click on "Start" button
+        PictureBox firstGuess;
+
+        int time = 60;
+
+
         About About = new About();
         string cardImageName = "image";
 
+        Timer clickTimer = new Timer();
+        //Timer timer = new Timer { Interval = 1000 };
+        Random Random = new Random();
 
         public MainPage() { InitializeComponent();
-            setQuastionIconAndNameToButtons();
+            
         }
-            /*
-                    Properties.Resources.Baby_Dack,
-                    Properties.Resources.Baby_Goose,
-                    Properties.Resources.Baby_Sheep,
-                    Properties.Resources.Bird,
-                    Properties.Resources.Cat,
-                    Properties.Resources.Chicken,
-                    Properties.Resources.cock,
-                    Properties.Resources.Cow,
-                    Properties.Resources.Dog,
-                    Properties.Resources.Goose,
-                    Properties.Resources.Horse,
-                    Properties.Resources.Rabbit,
-                    Properties.Resources.Pig,
-            */
+
+        private PictureBox[] pictureBoxes {
+            get
+            { return Controls.OfType<PictureBox>().ToArray(); }
+        }
+
+
+        private static IEnumerable<Image> images {
+            get {
+                return new Image[]
+                {
+                    Properties.Resources.image0,
+                    Properties.Resources.image1,
+                    Properties.Resources.image2,
+                    Properties.Resources.image3,
+                    Properties.Resources.image4,
+                    Properties.Resources.image5,
+                    Properties.Resources.image6,
+                    Properties.Resources.image7,
+                    Properties.Resources.image8,
+                    Properties.Resources.image9,
+                    Properties.Resources.image10,
+                    Properties.Resources.image11,
+                    Properties.Resources.image12,
+                };
+            }
+        }
 
         private void aboutStripMenuItem_Click(object sender, EventArgs e) { About.ShowDialog(); }
 
-        private void setQuastionIconAndNameToButtons() {
 
-            //Button button = new Button();
-            Random random = new Random();
-            Label label;
-            int cardRandomNumber;
-            int imageNumber;
+        private void startGameTimer()
+        {
+            timer.Start();
+            // delegate (below) = object-oriented function, similar to a pointer
+            timer.Tick += delegate
+            {
+                time--;
+                if (time < 0)
+                {
+                    timer.Stop(); // Ran out of time
+                    MessageBox.Show("Out of time");
+                    ResetImages(); // The game resets and user can choose to play again
+                }
 
+                var ssTime = TimeSpan.FromSeconds(time);
+                CountDownLabel.Text = "00:" + time.ToString(); // Displaying time in sec
+            };
+        }
 
-            for (int count = 0; count < cardTableLayoutPanel.Controls.Count; count++) {
-
-                if (cardTableLayoutPanel.Controls[count] is Label)
-                    label = (Label)cardTableLayoutPanel.Controls[count];
-                else
-                    continue;
+        private void ResetImages()
+        {
+            foreach (var pic in pictureBoxes)
+            {
+                pic.Tag = null;
+                pic.Visible = true;
             }
 
-
-            
-            //cardRandomNumber = random.Next(0, );
-            //cardRandomNumber = images;
-
-            
-        
-        
-        
+            HideImages();
+            setRandomImages();
+            time = 60;
+            timer.Start();
         }
+
+        private void HideImages()
+        {
+            foreach (var pic in pictureBoxes)
+            {
+                pic.Image = Properties.Resources.qm;
+            }
+        }
+
+
+        private PictureBox getFreeSlot()
+        {
+            int num;
+
+            do
+            {
+                num = Random.Next(0, pictureBoxes.Count());
+            }
+            while (pictureBoxes[num].Tag != null);
+            return pictureBoxes[num];
+        }
+
+
+
+        private void setRandomImages()
+        {
+            foreach (var image in images)
+            {
+                getFreeSlot().Tag = image;
+                getFreeSlot().Tag = image;
+            }
+
+        }
+
+        private void ClickTimer_Tick(object sender, EventArgs e)
+        {
+            HideImages();
+
+            allowClick = true;
+            clickTimer.Stop();
+        }
+
+        private void clickImage(object sender, EventArgs e)
+        {
+            if (!allowClick)
+                return;
+
+            var pic = (PictureBox)sender;
+
+            if (firstGuess == null)
+            {
+                firstGuess = pic;
+                pic.Image = (Image)pic.Tag;
+                return;
+            }
+
+            pic.Image = (Image)pic.Tag;
+            if (pic.Image == firstGuess.Image && pic != firstGuess)
+            {
+                pic.Visible = firstGuess.Visible = false;
+                {
+                    firstGuess = pic;
+                }
+                HideImages();
+
+            }
+            else
+            {
+                allowClick = false;
+                clickTimer.Start();
+            }
+
+            firstGuess = null;
+            if (pictureBoxes.Any(p => p.Visible))
+                return;
+            MessageBox.Show("You Win, Now Try Again");
+            ResetImages();
+        }
+
+        private void startGame(object sender, EventArgs e)
+        {
+            allowClick = true;
+            setRandomImages();
+            HideImages();
+            startGameTimer();
+            clickTimer.Interval = 1000;
+            clickTimer.Tick += ClickTimer_Tick;
+            startButton.Enabled = false;
+
+        }
+
+
+
 
 
     } // END OF MainPage Class
